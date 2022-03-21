9f5660c5763218d3149cc38a2b72e964853d367f
renesat <smol.ivan97@gmail.com>
2022-03-22 00:41:20 +0300

    Add correcto watch for config file and some fixes

--- src/Context.hs
+++ src/Context.hs
@@ -2,33 +2,35 @@
 
 module Context where
 
+import           Configs                        ( BlogConfig )
+import           Data.HashMap.Strict            ( toList )
+import           Data.Text                      ( Text
+                                                , unpack
+                                                )
 import           Hakyll
-import           Data.Text (unpack, Text)
-import           Configs ( BlogConfig)
-import           Data.HashMap.Strict (toList)
 
 ------------------------------------------
 -- Common contexts
 
 -- Blog context
 contextFromBlogConfig :: BlogConfig -> IO (Context String)
-contextFromBlogConfig blogConfig = return $
-  (foldl1 mappend $ map addField (toList blogConfig)) <>
-  activeClassField <>
-  defaultContext
-  where
-    addField :: (Text, Text) -> Context String
-    addField (name, value) = constField ("blog-" ++ (unpack name))
-                                        (unpack value)
+contextFromBlogConfig blogConfig =
+  return
+    $  (foldl1 mappend $ map addField (toList blogConfig))
+    <> activeClassField
+    <> defaultContext
+ where
+  addField :: (Text, Text) -> Context String
+  addField (name, value) = constField ("blog-" ++ (unpack name)) (unpack value)
 
 -- Base on https://groups.google.com/forum/#!searchin/hakyll/if$20class/hakyll/WGDYRa3Xg-w/nMJZ4KT8OZUJ
 activeClassField :: Context a
 activeClassField = functionField "activeClass" $ \[p] item -> do
   page <- getMetadataField (itemIdentifier item) "page-id" >>= pageExists
   return $ if page == p then "active" else page
-  where
-    pageExists (Just v) = return v
-    pageExists Nothing  = return "none"
+ where
+  pageExists (Just v) = return v
+  pageExists Nothing  = return "none"
 
 
 
@@ -37,26 +39,28 @@ activeClassField = functionField "activeClass" $ \[p] item -> do
 
 -- Post page
 postCtx :: Context String -> Context String
-postCtx context =
-    dateField "date" "%B %e, %Y" <>
-    mathCtx <>
-    context
+postCtx context = dateField "date" "%B %e, %Y" <> mathCtx <> context
 
 -- Archive page
-archiveCtx :: [Item (String, [Item String])] -> Context String -> Context String
+archiveCtx
+  :: [Item (String, [Item String])] -> Context String -> Context String
 archiveCtx years context =
-    listField "years" (  field "year" (return . fst . itemBody)
-                      <> listFieldWith "posts" (postCtx context) (return . snd . itemBody)
-                      )
-                      (return years) <>
-    context
+  listField
+      "years"
+      (  field "year" (return . fst . itemBody)
+      <> listFieldWith "posts" (postCtx context) (return . snd . itemBody)
+      )
+      (return years)
+    <> context
 
 -- Index page
 indexCtx :: [Item String] -> Context String -> Context String
 indexCtx posts context =
-    mathCtx <>
-    listField "posts" (teaserField "teaser" "_content" <> postCtx context) (return posts) <>
-    context
+  mathCtx
+    <> listField "posts"
+                 (teaserField "teaser" "_content" <> postCtx context)
+                 (return posts)
+    <> context
 
 ------------------------------------------
 -- Support contexts
@@ -68,17 +72,16 @@ mathCtx = field "katex-header" $ \item -> do
   katex <- getMetadataField (itemIdentifier item) "katex"
   return $ case katex of
     Just "false" -> ""
-    Just "off" -> ""
-    _ -> unlines [ "<link rel='stylesheet' href='/css/katex.min.css'>"
-                 , "<script type='text/javascript' src='/js/katex.min.js'></script>"
-                 , "<script src='/js/auto-render.min.js'></script>"
-                 ]
+    Just "off"   -> ""
+    _            -> unlines
+      [ "<link rel='stylesheet' href='/css/katex.min.css'>"
+      , "<script type='text/javascript' src='/js/katex.min.js'></script>"
+      , "<script src='/js/auto-render.min.js'></script>"
+      ]
 
 ------------------------------------------
 -- Feed functions
 
 -- Feed context
 feedCtx :: Context String -> Context String
-feedCtx context =
-  (postCtx context) <>
-  bodyField "description"
+feedCtx context = (postCtx context) <> bodyField "description"
--- src/Pygments/Pygments.hs
+++ src/Pygments/Pygments.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Pygments.Pygments (pygmentizeIO) where
 
-import           Hakyll (unixFilter, Compiler(..))
-import           Text.Pandoc.Definition (Block(..), Inline(..), Pandoc, Format(..))
+import           Hakyll (unixFilter, Compiler)
+import           Text.Pandoc.Definition (Block(..), Pandoc, Format(..))
 import           Text.Pandoc.Walk (walkM)
 import           Data.Char(toLower)
 import qualified Data.Text as T
@@ -18,5 +18,5 @@ pygmentize x = return x
 
 pygmentizeToHTML :: T.Text -> [T.Text] -> Compiler T.Text
 pygmentizeToHTML code options
-         | (length options) >= 1 = return . T.pack =<< unixFilter "pygmentize" ["-l", (map toLower (T.unpack (head options))),  "-f", "html"] (T.unpack code)
+         | not (null options) = T.pack <$> unixFilter "pygmentize" ["-l", map toLower (T.unpack (head options)),  "-f", "html"] (T.unpack code)
          | otherwise = return $ T.pack $ "<div class =\"highlight\"><pre>" ++ T.unpack code ++ "</pre></div>"
