5427b719b56ad4a13de35dc2ccfce9f1c7fabce5
renesat <smol.ivan97@gmail.com>
2023-04-26 21:15:33 +0200

    Update builds
    - Update flake
    - update lts

--- flake.nix
+++ flake.nix
@@ -18,23 +18,47 @@
           sandbox = false;
         };
 
-        package = pkgs.haskell.lib.buildStackProject {
-          ghc = pkgs.ghc;
-          name = "IDEAGlueBlog";
-          buildInputs = with pkgs; [
-            libffi
-            gmp
-            zlib
+        stack-nix-integration = pkgs.writeText "stack-nix-integration.nix" ''
+          {ghc}:
+          let
+            pkgs = import ${pkgs.path} {};
+          in pkgs.haskell.lib.buildStackProject {
+            inherit ghc;
+            name = "IDEAGlueBlog";
+            buildInputs = with pkgs; [
+              libffi
+              gmp
+              zlib
 
-            inkscape
-            nodePackages.katex
-            imagemagick
-            libaom
-            python3Packages.pygments
-          ];
+              inkscape
+              nodePackages.katex
+              imagemagick
+              libaom
+              python3Packages.pygments
+
+            ];
+          }
+        '';
+
+        # Source: https://www.tweag.io/blog/2022-06-02-haskell-stack-nix-shell/
+        stack-wrapped = pkgs.symlinkJoin {
+          name = "stack";
+          paths = [ pkgs.stack ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/stack \
+              --add-flags "\
+                --nix \
+                --no-nix-pure \
+                --nix-shell-file="${stack-nix-integration}"\
+              "
+          '';
         };
+
       in {
-        defaultPackage = package;
-        devShell = package;
+        devShell = pkgs.mkShell {
+          buildInputs = [ stack-wrapped ];
+          NIX_PATH = "nixpkgs=" + pkgs.path;
+        };
       });
 }
