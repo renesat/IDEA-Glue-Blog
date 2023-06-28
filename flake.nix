f4447d7331d4c9cb48ebf369a65f70dbc518dda7
renesat <smol.ivan97@gmail.com>
2023-06-28 20:53:17 +0200

    Update flake and ci

--- flake.nix
+++ flake.nix
@@ -1,132 +1,145 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
+    flake-root.url = "github:srid/flake-root";
+    mission-control.url = "github:Platonic-Systems/mission-control";
   };
 
-  outputs = {
+  outputs = inputs @ {
     self,
-    nixpkgs,
-    flake-utils,
+    flake-parts,
+    ...
   }:
-    flake-utils.lib.eachSystem [
-      "aarch64-linux"
-      "aarch64-darwin"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ] (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        sandbox = false;
-      };
-      catppuccin = with pkgs.python3.pkgs;
-        buildPythonPackage rec {
-          pname = "catppuccin";
-          version = "1.2.0";
-          src = pkgs.python3.pkgs.fetchPypi {
-            inherit pname version;
-            sha256 = "sha256-hUNt6RHntQzamDX1SdhBzSj3pR/xxb6lpwzvYnqwOIo=";
-          };
-          passthru.optional-dependencies = {pygments = [pygments];};
-          doCheck = false;
-        };
-
-      blog-builder =
-        pkgs.haskellPackages.callCabal2nixWithOptions "IDEA-Glue-Blog-Builder"
-        (pkgs.lib.cleanSourceWith {
-          filter = path: type: let
-            relPath =
-              pkgs.lib.removePrefix (toString self + "/") (toString path);
-          in
-            !(pkgs.lib.hasPrefix "site/" relPath);
-          src = self;
-        }) "--hpack" {};
-
-      buildInputs = with pkgs; [
-        inkscape
-        imagemagick
-        (python3.withPackages (ps: [ps.pygments catppuccin]))
-        nodePackages.katex
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.haskell-flake.flakeModule
+        inputs.treefmt-nix.flakeModule
+        inputs.flake-root.flakeModule
+        inputs.mission-control.flakeModule
       ];
-      blog = pkgs.stdenvNoCC.mkDerivation {
-        name = "IDEA-Glue-Blog";
-        src = pkgs.lib.cleanSource self;
-        nativeBuildInputs = buildInputs;
-        buildPhase = ''
-          ${blog-builder}/bin/site build
-        '';
-        installPhase = ''
-          mkdir -p $out
-          cp -r build/site $out/public
-        '';
-        LANG = "C.UTF-8";
-      };
+      systems = [
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      perSystem = {
+        self',
+        system,
+        lib,
+        config,
+        pkgs,
+        ...
+      }: let
+        catppuccin = with pkgs.python3.pkgs;
+          buildPythonPackage rec {
+            pname = "catppuccin";
+            version = "1.2.0";
+            src = pkgs.python3.pkgs.fetchPypi {
+              inherit pname version;
+              sha256 = "sha256-hUNt6RHntQzamDX1SdhBzSj3pR/xxb6lpwzvYnqwOIo=";
+            };
+            passthru.optional-dependencies = {pygments = [pygments];};
+            doCheck = false;
+          };
 
-      blog-dev = blog.overrideAttrs (old: rec {src = self;});
+        buildInputs = with pkgs; [
+          inkscape
+          imagemagick
+          (python3.withPackages (ps: [ps.pygments catppuccin]))
+          nodePackages.katex
+        ];
 
-      stack-nix-integration = pkgs.writeText "stack-nix-integration.nix" ''
-        {ghc}:
-        let
-          pkgs = import ${pkgs.path} {};
-        in pkgs.haskell.lib.buildStackProject {
-          inherit ghc;
-          name = "IDEAGlueBlog";
-          buildInputs = with pkgs; [
-            libffi
-            gmp
-            zlib
-
-            inkscape
-            nodePackages.katex
-            imagemagick
-            libaom
-            python3Packages.pygments
-
-          ];
-        }
-      '';
-
-      # Source: https://www.tweag.io/blog/2022-06-02-haskell-stack-nix-shell/
-      stack-wrapped = pkgs.symlinkJoin {
-        name = "stack";
-        paths = [pkgs.stack];
-        buildInputs = [pkgs.makeWrapper];
-        postBuild = ''
-          wrapProgram $out/bin/stack \
-            --add-flags "\
-              --nix \
-              --no-nix-pure \
-              --nix-shell-file="${stack-nix-integration}"\
-            "
-        '';
-      };
-    in {
-      packages = {
-        inherit blog blog-builder blog-dev;
-        default = blog-builder;
-      };
-      apps = {
-        builder = {
-          type = "app";
-          program = "${blog-builder}/bin/site";
+        blog = pkgs.stdenvNoCC.mkDerivation {
+          name = "IDEA-Glue-Blog_static";
+          src = pkgs.lib.cleanSource self;
+          nativeBuildInputs = buildInputs;
+          buildPhase = ''
+            ${self'.apps.site.program} build
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp -r build/site $out/public
+          '';
+          LANG = "C.UTF-8";
         };
-        defalut = self.apps."${system}".builder;
-      };
-      devShells = {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs;
-            [
-              stack-wrapped
+        blog-dev = blog.overrideAttrs (old: rec {src = self;});
+      in {
+        haskellProjects.default = {
+          devShell = {
+            # TODO: Remove this after https://github.com/numtide/treefmt-nix/issues/65
+            tools = hp:
+              {
+                hpack = hp.hpack;
+                treefmt = config.treefmt.build.wrapper;
+              }
+              // config.treefmt.build.programs;
+            hlsCheck.enable = false;
+          };
+          packages = {
+          };
+          autoWire = ["packages" "apps" "checks"];
+        };
 
-              haskellPackages.hasktags
-              haskellPackages.fourmolu
-              haskell-language-server
-              alejandra
-              html-tidy
+        treefmt.config = {
+          inherit (config.flake-root) projectRootFile;
+          package = pkgs.treefmt;
+
+          programs.ormolu.enable = true;
+          programs.ormolu.package = pkgs.haskellPackages.fourmolu_0_12_0_0.overrideScope (lself: lsuper: {
+            Cabal-syntax = lself.Cabal-syntax_3_10_1_0;
+            ghc-lib-parser = lself.ghc-lib-parser_9_6_2_20230523;
+            parsec = lself.parsec_3_1_16_1;
+            text = lself.text_2_0_2;
+          });
+          programs.alejandra.enable = true;
+          programs.hlint.enable = true;
+        };
+
+        mission-control.scripts = {
+          repl = {
+            description = "Start the cabal repl";
+            exec = ''
+              cabal repl "$@"
+            '';
+            category = "Dev Tools";
+          };
+          fmt = {
+            description = "Format the source tree";
+            exec = config.treefmt.build.wrapper;
+            category = "Dev Tools";
+          };
+          build = {
+            description = "Build all";
+            exec = ''
+              cabal build
+            '';
+            category = "Dev Tools";
+          };
+        };
+
+        packages = {
+          inherit blog blog-dev;
+          default = self'.packages.IDEA-Glue-Blog;
+        };
+        apps = {
+          default = self'.apps.site;
+        };
+
+        devShells.default = pkgs.mkShell {
+          name = "IDEA-Glue-Blog";
+          inputsFrom =
+            [
+              config.haskellProjects.default.outputs.devShell
+              config.flake-root.devShell
+              config.mission-control.devShell
             ]
             ++ buildInputs;
-          NIX_PATH = "nixpkgs=" + pkgs.path;
         };
       };
-    });
+    };
 }
